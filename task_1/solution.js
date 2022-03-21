e39ddeec843155ee1de642fb9f88ebcb20c14653
VenMin <100590841+VenMin@users.noreply.github.com>
2022-03-21 20:00:21 +0300

    Add files via upload

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,38 +1,55 @@
-// Задание №1.1. Оповещение о добавлении в корзину
+function addInCartMessage(title, price) {
+    let productName = title;
+    let productPrice = price;
 
-function addInCartMessage(productName, productPrice){
+    // Задание №1.1. Оповещение о добавлении в корзину
 
-		let message;
-			message = productName;
-			message += ' ';
-			message += 'за';
-			message += ' ';
-			message += productPrice;
-			message += ' ';
-			message += 'теперь в корзине!';
-
-		console.log(message);
+    let message;
+    message=productName
+    message +=' ';
+    message +='за';
+    message +=' ';
+    message +=productPrice;
+    message +=' ';
+    message +='теперь в корзине!'
+    
+	console.log (message);
 }
-//Конец решения задания №1.1
 
-// Задание №1.2. Увеличение количества в корзине
-function addInCartCountChange(){
-		let oldValue =0; 
-		let newValue;
+    // Конец решения задания №1.1.
 
-		newValue = oldValue;
-		newValue ++;
+    return message;
 }
-// Конец решения задания №1.2
 
-// Задание №1.3. Увеличение суммы в корзине
-function addInCartSumChange(difference){
-		let oldSum =0;
-		let newSum = oldSum + difference;
-		let newSumText = newSum + ' ₽';
+function addInCartCountChange(value) {
 
-		newSumText = `${newSum} ₽`; 
+    // Задание №1.2. Увеличение количества в корзине
+    let oldValue = value;
 
-
+    let newValue;
+    newValue=oldValue
+	newValue ++;
 }
-// Конец решения задания №1.3
+    
+
+    // Конец решения задания №1.2.
+
+    return newValue;
+}
+
+function addInCartSumChange(sum, delta) {
+    let oldSum = sum;
+    let difference = delta;
+
+    // Задание №1.3. Увеличение суммы в корзине
+
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽';
+	newSumText = `${newSum} ₽`; 
+}
+
+    // Конец решения задания №1.3.
+
+    return newSumText;
+}
+
