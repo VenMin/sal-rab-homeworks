a9a59fd7b85115d97d53bba9eeadbc82a8e75d09
VenMin <100590841+VenMin@users.noreply.github.com>
2022-04-02 12:29:33 +0300

    Add files via upload

---
+++ task_1/solution.js
@@ -0,0 +1,55 @@
+function addInCartMessage(title, price) {
+    let productName = title;
+    let productPrice = price;
+
+    // Задание №1.1. Оповещение о добавлении в корзину
+
+    let message;
+    message = productName
+    message += ' ';
+    message += 'за';
+    message += ' ';
+    message += productPrice;
+    message += ' ';
+    message += 'теперь в корзине!';
+    
+	console.log (message);
+
+
+    // Конец решения задания №1.1.
+
+    return message;
+}
+
+function addInCartCountChange(value) {
+
+    // Задание №1.2. Увеличение количества в корзине
+    let oldValue = value;
+
+    let newValue;
+    newValue = oldValue;
+	newValue ++;
+
+    
+
+    // Конец решения задания №1.2.
+
+    return newValue;
+}
+
+function addInCartSumChange(sum, delta) {
+    let oldSum = sum;
+    let difference = delta;
+
+    // Задание №1.3. Увеличение суммы в корзине
+
+    let newSum = oldSum + difference;
+    let newSumText = newSum + '₽';
+	newSumText = ${newSum} '₽'; 
+
+
+    // Конец решения задания №1.3.
+
+    return newSumText;
+}
+
