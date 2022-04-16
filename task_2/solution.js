ef8a2f6b745fcce48a22a417af9f1e26929ee90f
VenMin <100590841+VenMin@users.noreply.github.com>
2022-04-16 09:59:42 +0300

    Add files via upload

---
+++ task_2/solution.js
@@ -0,0 +1,51 @@
+function calcShipping(sum, min, shipping) {
+    let productsSum = sum; // сумма в корзине
+    let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
+    let shippingPrice = shipping; // стоимость доставки
+
+    // Задание №2.1. Рассчитать доставку
+      if( productsSum == 0 || productsSum >= freeShippingMinSum ) 
+	{
+	    shippingSum = 0;
+    }
+	    else 
+	        shippingSum = shippingPrice;
+    // Конец решения задания №2.1.
+    return shippingSum;
+    }
+
+function calcDiscount(sum, min, discount) {
+    let productsSum = sum; // сумма в корзине
+    let discountMinSum = min; // минимальная цена для скидки
+    let discountPart = discount; // величина скидки в процентах
+
+    // Задание №2.2. Рассчитать скидку
+
+	
+    let discountSum =productsSum >= discountMinSum ? productsSum * discountPart / 100 : 0;
+
+    // Конец решения задания №2.2.
+
+    return discountSum;
+    }
+
+function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
+    let productsSum = sum;
+    let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+	
+
+    // Задача №2.3. Рассчитать скидки и доставку в корзине
+
+    let totalSum;
+	  totalSum = productsSum;
+	  totalSum -= discountSum;
+	
+    let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+      totalSum += shippingSum
+    let freeShipping= shippingSum == 0;
+      
+
+    // Конец решения задачи №2.3.
+
+    return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
+    }
